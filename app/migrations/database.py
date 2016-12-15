99c15ba2d8e2f12a4b4045f7199b8d9a67bc6169
YuriyOrlov <navuchodonsr@yandex.ru>
2016-12-15 21:25:42 +0300

    Changed code according to PEP8

--- app/migrations/database.py
+++ app/migrations/database.py
@@ -10,47 +10,46 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 estate_items = db.Table('estate_items',
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)  
-    obj_type = db.Column(db.String(5))
-    name = db.Column(db.Text)
-    obj_address = db.Column(db.String(250))
-    metro_station = db.Column(db.String(150))
-    rooms = db.Column(db.Integer)
-    area = db.Column(db.Float)
-    href = db.Column(db.String(250), unique = True)
-    source = db.Column(db.String(50))
-    floor = db.Column(db.Integer)
-    lat = db.Column(db.Float)
-    lng = db.Column(db.Float)
-    is_valid = db.Column(db.Boolean))
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    obj_type=db.Column(db.String(5))
+    name=db.Column(db.Text)
+    obj_address=db.Column(db.String(250))
+    metro_station=db.Column(db.String(150))
+    rooms=db.Column(db.Integer)
+    area=db.Column(db.Float)
+    href=db.Column(db.String(250), unique=True)
+    source=db.Column(db.String(50))
+    floor=db.Column(db.Integer)
+    lat=db.Column(db.Float)
+    lng=db.Column(db.Float)
+    is_valid=db.Column(db.Boolean))
 
-Date_and_price = db.Table('date_and_price',
-	id = db.Column(db.Integer, primary_key = True, autoincrement = True)  
-    price = db.Column(db.Integer)
-    date_of_creation = db.Column(db.DateTime)
-    date_of_parsing = db.Column(db.DateTime)
-    object_id = db.Column(db.Integer, db.ForeignKey('estate_items.id')))
+Date_and_price = db.Table('date_and_price', 
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    price=db.Column(db.Integer)
+    date_of_creation=db.Column(db.DateTime)
+    date_of_parsing=db.Column(db.DateTime)
+    object_id=db.Column(db.Integer, db.ForeignKey('estate_items.id')))
 
 class Item(db.Model):
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)  
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     obj_type = db.Column(db.String(5))
     name = db.Column(db.Text)
     obj_address = db.Column(db.String(250))
     metro_station = db.Column(db.String(150))
     rooms = db.Column(db.Integer)
     area = db.Column(db.Float)
-    href = db.Column(db.String(250), unique = True)
+    href = db.Column(db.String(250), unique=True)
     source = db.Column(db.String(50))
     floor = db.Column(db.Integer)
     lat = db.Column(db.Float)
     lng = db.Column(db.Float)
     is_valid = db.Column(db.Boolean)
-    
-    #relashion_btw_date_price_object = relationship('Date_and_price', backref ='rel_date_price')
 
-    def __init__ (self, obj_type = None, name = None, obj_address = None, metro_station = None, 
-        rooms = None, area = None, href = None, source = None, floor = None, lat = None, lng = None, is_valid = True):
-        
+
+    def __init__(self, obj_type=None, name=None, obj_address=None, metro_station=None,
+        rooms=None, area=None, href=None, source=None, floor=None, lat=None, lng=None, is_valid=True):
+
         self.obj_type = obj_type
         self.name = name
         self.obj_address = obj_address
@@ -65,26 +64,27 @@ class Item(db.Model):
         self.is_valid = is_valid
 
     def __repr__(self):
-        return '{} {} {} {} {} {} {} {} {} {} {}'.format(self.obj_type, self.name, self.obj_address, self.metro_station, 
+        return '{} {} {} {} {} {} {} {} {} {} {}'.format(self.obj_type, self.name, self.obj_address, self.metro_station,
             self.rooms, self.area, self.href, self.source, self.floor, self.lat, self.lng, self.is_valid)
-        
+
+
 class Date_and_price(db.Model):
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)  
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     price = db.Column(db.Integer)
     date_of_creation = db.Column(db.DateTime)
     date_of_parsing = db.Column(db.DateTime)
     object_id = db.Column(db.Integer, db.ForeignKey('estate_items.id'))
-    
 
-    def __init__(self, price = None, date_of_creation = None, date_of_parsing = None,  object_id = None):
+
+    def __init__(self, price=None, date_of_creation=None, date_of_parsing=None, object_id=None):
         self.price = price
         self.date_of_creation = date_of_creation
         self.date_of_parsing = date_of_parsing
         self.object_id = object_id
 
     def __repr__(self):
-        return '{} {}'.format(self.price,self.date_of_creation)
-        
+        return '{} {}'.format(self.price, self.date_of_creation)
+
 
 if __name__ == '__main__':
     db.create_all()
